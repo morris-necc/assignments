3d9251f2d78f47e1bce48a6757c8557810c8c007
morris-necc <71365308+morris-necc@users.noreply.github.com>
2020-09-21 13:49:36 +0700

    Add files via upload

---
+++ 21-09-2020/number1.py
@@ -0,0 +1,5 @@
+student1 = int(input("Enter the first student's score: "))
+student2 = int(input("Enter the second student's score: "))
+student3 = int(input("Enter the third student's score: "))
+avg = (student1 + student2 + student3)/3
+print("Average",avg)
---
+++ 21-09-2020/number2.py
@@ -0,0 +1,3 @@
+degrees = int(input("Insert angle: "))
+radians = degrees * 3.14 / 180
+print("angle is",degrees,"degrees, or",radians,"radians")
