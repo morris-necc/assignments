3ea99aabb3ec6e801dcc47f408d47c2a50422525
morris-necc <71365308+morris-necc@users.noreply.github.com>
2020-09-21 14:30:14 +0700

    Update number2.py

--- 21-09-2020/number2.py
+++ 21-09-2020/number2.py
@@ -1,3 +1,9 @@
-degrees = int(input("Insert angle: "))
-radians = degrees * 3.14 / 180
-print("angle is",degrees,"degrees, or",radians,"radians")
+student1 = int(input("Enter the first student's score: "))
+student2 = int(input("Enter the second student's score: "))
+student3 = int(input("Enter the third student's score: "))
+avg = (student1 + student2 + student3)
+print("Student scores:\n",
+    student1, "\n",
+    student2, "\n",
+    student3, "\n",
+    "Average:", avg)
