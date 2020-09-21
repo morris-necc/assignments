32b45437b755e166a2041a05a4150cbd89163ce4
morris-necc <71365308+morris-necc@users.noreply.github.com>
2020-09-21 14:30:43 +0700

    Update number2.py

--- 21-09-2020/number2.py
+++ 21-09-2020/number2.py
@@ -1,7 +1,7 @@
 student1 = int(input("Enter the first student's score: "))
 student2 = int(input("Enter the second student's score: "))
 student3 = int(input("Enter the third student's score: "))
-avg = (student1 + student2 + student3)
+avg = (student1 + student2 + student3) / 3
 print("Student scores:\n",
     student1, "\n",
     student2, "\n",
