01f6063d1faa759b5e2e01d38e7d47778a1c0ec6
Morris Kim <morris.kim@binus.ac.id>
2020-10-05 14:49:08 +0700

    re-added some files

---
+++ 21-09-2020/number1.py
@@ -0,0 +1,4 @@
+degrees = int(input("Insert angle: "))
+radians = degrees * 3.14 / 180
+print("Degrees:", degrees, "\n"
+    "Radians:", radians)
---
+++ 21-09-2020/number5.py
@@ -0,0 +1,7 @@
+velocity = float(input("Please enter velocity: "))
+frequency = float(input("Please enter frequency: "))
+wavelength = velocity/frequency
+
+print("The speed (m/s):", velocity, "\n"
+    "The frequency (Hz):", frequency, "\n"
+    "The wavelength (m):", wavelength)
