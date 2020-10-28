b892637240713e0676987caf3d9c65943ff33d81
Morris Kim <morris.kim@binus.ac.id>
2020-10-28 22:21:50 +0700

    One big commit for all the assignments

--- Week5/12-10-2020/number5.py
+++ Week5/12-10-2020/number5.py
@@ -6,5 +6,8 @@ def fibonacci(x):
     else:
         return(fibonacci(x-1)+fibonacci(x-2))
 
-fibonacci_series = lambda x: [fibonacci(i) for i in range(1,x+1)]
-print(fibonacci_series(10)) #test
+fibonacci_series = lambda x: 0 if x==1 else(x-1 if x==1 or x==2 else fibonacci_series(x-1)+fibonacci_series(x-2))
+print_series = lambda function, length: [function(i) for i in range(1,length+1)]
+print(print_series(fibonacci_series, 10))
+fibonacci_2 = lambda y: [fibonacci(i) for i in range(1,y+1)]
+print(fibonacci_2(10))
